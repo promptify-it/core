7309dcfa3c417f93daaaa46a79cbb7ae65323203
danieletulone <danieletulone.work@gmail.com>
2024-04-29 10:28:42 +0200

    feat(package): bind loader, persistent-storage and autobind nodeData and nodeContentData classes

--- src/PromptifyItServiceProvider.php
+++ src/PromptifyItServiceProvider.php
@@ -2,24 +2,88 @@
 
 namespace PromptifyIt\PromptifyIt;
 
-use PromptifyIt\PromptifyIt\Commands\PromptifyItCommand;
+use PromptifyIt\PromptifyIt\PersistentStorage\JsonPersistentStorage;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
 class PromptifyItServiceProvider extends PackageServiceProvider
 {
+    /**
+     * Configure the package.
+     */
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('promptify-it')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_promptify-it_table')
-            ->hasCommand(PromptifyItCommand::class);
+            ->hasConfigFile();
+    }
+
+    /**
+     * Register the package services.
+     */
+    public function registeringPackage()
+    {
+        $this
+            ->bindLoader()
+            ->bindPersistentStorage()
+            ->bindNodesUsingType();
+    }
+
+    /**
+     * Bind the loader.
+     */
+    public function bindLoader(): self
+    {
+        $this->app->bind(Contracts\Loader::class, Loaders\RemoteLoader::class);
+
+        return $this;
+    }
+
+    /**
+     * Bind the persistent storage.
+     */
+    public function bindPersistentStorage(): self
+    {
+        $this->app->singleton(Contracts\PersistentStorage::class, JsonPersistentStorage::class);
+
+        return $this;
+    }
+
+    public function bindNodesUsingType(): self
+    {
+        $nodeClasses = $this->autoloadNodes();
+
+        $this->app->bind('promptify-it.nodes', fn () => $nodeClasses);
+
+        return $this;
+    }
+
+    /**
+     * Autoload the nodes scanning recursively the nodes directory.
+     */
+    private function autoloadNodes(): array
+    {
+        $nodes = [];
+
+        $directory = __DIR__ . '/Data/Nodes';
+
+        $files = scandir($directory);
+
+        foreach ($files as $file) {
+            if ($file == '.' || $file == '..') {
+                continue;
+            }
+
+            if (is_file($directory . '/' . $file)) {
+                continue;
+            }
+
+            $nodes[str($file)->camel()->toString()] = [
+                'nodeData' => 'PromptifyIt\\PromptifyIt\\Data\\Nodes\\' . $file . '\\' . $file . 'NodeData',
+                'nodeContentData' => 'PromptifyIt\\PromptifyIt\\Data\\Nodes\\' . $file . '\\' . $file . 'NodeContentData',
+            ];
+        }
+
+        return $nodes;
     }
 }
