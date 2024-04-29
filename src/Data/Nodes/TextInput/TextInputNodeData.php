3cb47a21e2f07cfcad4ae3b14baa5e2f5a368cf5
danieletulone <danieletulone.work@gmail.com>
2024-04-29 10:27:28 +0200

    feat(nodes): add text-input node

---
+++ src/Data/Nodes/TextInput/TextInputNodeData.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace PromptifyIt\PromptifyIt\Data\Nodes\TextInput;
+
+use PromptifyIt\PromptifyIt\Contracts\Executable;
+use PromptifyIt\PromptifyIt\Data\Nodes\NodeData;
+
+use function Laravel\Prompts\text;
+
+/**
+ * @property TextInputNodeContentData $content
+ */
+class TextInputNodeData extends NodeData implements Executable
+{
+    public function execute(&$data): void
+    {
+        $value = text(
+            $this->content->label,
+            $this->content->placeholder,
+            $this->resolveDefault($data),
+            $this->content->required,
+            $this->content->validate,
+            $this->content->hint
+        );
+
+        $data[$this->content->key] = $value;
+
+        if (is_string($value)) {
+            $this->provideReplacersFor($this->content->key, $value, $data);
+        }
+    }
+
+    private function resolveDefault($data): ?string
+    {
+        if ($this->content->default !== null) {
+            return $this->replaceWithVariables($this->content->default, $data);
+        }
+
+        return $this->content->default;
+    }
+}
