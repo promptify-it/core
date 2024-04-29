6f8f1de851bbda699eb7bef218bb35b59dff1638
danieletulone <danieletulone.work@gmail.com>
2024-04-29 10:27:07 +0200

    feat(nodes): add select-input node

---
+++ src/Data/Nodes/SelectInput/SelectInputNodeContentData.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace PromptifyIt\PromptifyIt\Data\Nodes\SelectInput;
+
+use Illuminate\Support\Collection;
+use Spatie\LaravelData\Data;
+
+class SelectInputNodeContentData extends Data
+{
+    public function __construct(
+        public string $key,
+        public string $label,
+        public array $options,
+        public null|int|string $default = null,
+        public int $scroll = 5,
+        public mixed $validate = null,
+        public string $hint = '',
+        public bool|string $required = true
+    ) {
+        //
+    }
+}
