ef76e36053324a18d3e96c44a567036a8bd502dd
danieletulone <danieletulone.work@gmail.com>
2024-05-20 15:47:00 +0200

    feat: add ArrayToString cast to script in ShellNodeContentData

--- src/Data/Nodes/Shell/ShellNodeContentData.php
+++ src/Data/Nodes/Shell/ShellNodeContentData.php
@@ -2,11 +2,14 @@
 
 namespace PromptifyIt\PromptifyIt\Data\Nodes\Shell;
 
+use PromptifyIt\PromptifyIt\Castables\ArrayToString;
+use Spatie\LaravelData\Attributes\WithCast;
 use Spatie\LaravelData\Data;
 
 class ShellNodeContentData extends Data
 {
     public function __construct(
+        #[WithCast(ArrayToString::class)]
         public string $script,
     ) {
         //
