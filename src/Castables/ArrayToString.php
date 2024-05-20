fdc2379f9b7715ce412792a90fd7f6e5eff31e30
danieletulone <danieletulone@users.noreply.github.com>
2024-05-20 14:27:25 +0000

    Fix styling

--- src/Castables/ArrayToString.php
+++ src/Castables/ArrayToString.php
@@ -39,13 +39,15 @@ class ArrayToString implements Cast
         return file_get_contents($url);
     }
 
-    function getSSLPage($url) {
+    public function getSSLPage($url)
+    {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_HEADER, false);
         curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_SSLVERSION,3);
+        curl_setopt($ch, CURLOPT_SSLVERSION, 3);
         $result = curl_exec($ch);
         curl_close($ch);
+
         return $result;
     }
 }
