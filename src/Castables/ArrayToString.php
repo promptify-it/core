bc83aa74e3518b414935371e409a73e998a5c66f
danieletulone <danieletulone.work@gmail.com>
2024-05-20 17:19:14 +0200

    feat: use Http::withoutVerifying to download files

--- src/Castables/ArrayToString.php
+++ src/Castables/ArrayToString.php
@@ -2,6 +2,7 @@
 
 namespace PromptifyIt\PromptifyIt\Castables;
 
+use Illuminate\Support\Facades\Http;
 use Spatie\LaravelData\Casts\Cast;
 use Spatie\LaravelData\Support\Creation\CreationContext;
 use Spatie\LaravelData\Support\DataProperty;
@@ -32,22 +33,6 @@ class ArrayToString implements Cast
 
     private function downloadFileText(string $url): string
     {
-        if (strpos($url, 'https') === 0) {
-            return $this->getSSLPage($url);
-        }
-
-        return file_get_contents($url);
-    }
-
-    public function getSSLPage($url)
-    {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_HEADER, false);
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_SSLVERSION, 3);
-        $result = curl_exec($ch);
-        curl_close($ch);
-
-        return $result;
+        return Http::withoutVerifying()->get($url)->body();
     }
 }
