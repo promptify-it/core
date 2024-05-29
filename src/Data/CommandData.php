a7151e2feede111066316cc06d1b3400fcbbf223
Daniele Tulone <35412801+danieletulone@users.noreply.github.com>
2024-05-29 15:16:16 +0200

    feat: add CommandOptionsData

--- src/Data/CommandData.php
+++ src/Data/CommandData.php
@@ -4,14 +4,19 @@ namespace PromptifyIt\PromptifyIt\Data;
 
 use PromptifyIt\PromptifyIt\Data\Nodes\RootNodeData;
 use Spatie\LaravelData\Data;
+use PromptifyIt\PromptifyIt\Data\CommandOptionsData;
+use Spatie\LaravelData\Optional;
 
 class CommandData extends Data
 {
+    public CommandOptionsData|Optional $options;
+
     public function __construct(
+        public string $id,
         public string $signature,
         public string $description,
         public RootNodeData $root,
     ) {
-        //
+        $this->options = new CommandOptionsData();
     }
 }
